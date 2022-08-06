479635aa4fa17617b850d4c9c90fc14a252646d7
Amine Boukhari <aminetech21@outlook.com>
2022-08-06 21:56:38 +0700

    feat: initial webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/main.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx',]
+  },
+  stats: {
+    children: true,
+  },
+  module: {
+    rules: [
+        {
+            test: /\.(ts|js)x?$/,
+            exclude: /node_modules/,
+            use: 'babel-loader'
+        }
+    ],
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, './dist'),
+    },
+    compress: true,
+    port: 8000,
+  },
+  devtool: 'source-map',
+  plugins: [new HtmlWebpackPlugin({
+      template: "./src/public/index.html",
+    }), new ForkTsCheckerWebpackPlugin()],
+};
